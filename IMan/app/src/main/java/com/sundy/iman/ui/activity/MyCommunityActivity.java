2bb7947dbdbd445224b95a00f8ede0d27e9098ce
Sundy <308379285@qq.com>
2017-10-16 20:52:55 +0800

    完成我的社区列表页面

--- IMan/app/src/main/java/com/sundy/iman/ui/activity/MyCommunityActivity.java
+++ IMan/app/src/main/java/com/sundy/iman/ui/activity/MyCommunityActivity.java
@@ -1,20 +1,37 @@
 package com.sundy.iman.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.orhanobut.logger.Logger;
 import com.sundy.iman.R;
+import com.sundy.iman.config.Constants;
+import com.sundy.iman.entity.CommunityItemEntity;
 import com.sundy.iman.entity.CommunityListEntity;
 import com.sundy.iman.interfaces.OnTitleBarClickListener;
 import com.sundy.iman.net.ParamHelper;
 import com.sundy.iman.net.RetrofitCallback;
 import com.sundy.iman.net.RetrofitHelper;
+import com.sundy.iman.paperdb.PaperUtils;
+import com.sundy.iman.view.CustomLoadMoreView;
 import com.sundy.iman.view.TitleBarView;
+import com.sundy.iman.view.WrapContentLinearLayoutManager;
+import com.zhy.view.flowlayout.FlowLayout;
+import com.zhy.view.flowlayout.TagAdapter;
+import com.zhy.view.flowlayout.TagFlowLayout;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
@@ -35,6 +52,13 @@ public class MyCommunityActivity extends BaseActivity {
     @BindView(R.id.rv_community)
     RecyclerView rvCommunity;
 
+    private String keyword = "";
+    private int page = 1; //当前页码
+    private int perpage = 10; //每页显示条数
+    private boolean canLoadMore = true;
+    private MyCommunityAdapter communityAdapter;
+    private List<CommunityItemEntity> listCommunity = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +67,9 @@ public class MyCommunityActivity extends BaseActivity {
 
         initTitle();
         init();
+        if (listCommunity != null)
+            listCommunity.clear();
+        getCommunityList();
     }
 
     private void initTitle() {
@@ -76,24 +103,46 @@ public class MyCommunityActivity extends BaseActivity {
     }
 
     private void init() {
-        rvCommunity.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        rvCommunity.setLayoutManager(new WrapContentLinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        communityAdapter = new MyCommunityAdapter(R.layout.item_my_community, listCommunity);
+
+        communityAdapter.openLoadAnimation();
+        communityAdapter.isFirstOnly(false);
+        communityAdapter.setLoadMoreView(new CustomLoadMoreView());
+        communityAdapter.setEnableLoadMore(true);
+        communityAdapter.setOnLoadMoreListener(onLoadMoreListener, rvCommunity);
+
+        rvCommunity.setAdapter(communityAdapter);
     }
 
     //获取社区列表
     private void getCommunityList() {
         Map<String, String> param = new HashMap<>();
-        param.put("type", ""); //类型: 1-全部社区，2-我的社区
-        param.put("mid", "");
-        param.put("session_key", "");
-        param.put("keyword", "");
-        param.put("page", ""); //当前页码
-        param.put("perpage", ""); //每页显示条数
+        param.put("type", "2"); //1-全部社区, 2-我的社区, 3-发布广告的社区搜索, 4-加入推广社区搜索
+        param.put("mid", PaperUtils.getMId());
+        param.put("session_key", PaperUtils.getSessionKey());
+        param.put("keyword", keyword);
+        param.put("tags", "");
+        param.put("province", "");
+        param.put("city", "");
+        param.put("page", page + ""); //当前页码
+        param.put("perpage", perpage + ""); //每页显示条数
         Call<CommunityListEntity> call = RetrofitHelper.getInstance().getRetrofitServer()
                 .getCommunityList(ParamHelper.formatData(param));
         call.enqueue(new RetrofitCallback<CommunityListEntity>() {
             @Override
             public void onSuccess(Call<CommunityListEntity> call, Response<CommunityListEntity> response) {
-
+                CommunityListEntity communityListEntity = response.body();
+                if (communityListEntity != null) {
+                    int code = communityListEntity.getCode();
+                    String msg = communityListEntity.getMsg();
+                    if (code == Constants.CODE_SUCCESS) {
+                        CommunityListEntity.DataEntity dataEntity = communityListEntity.getData();
+                        if (dataEntity != null) {
+                            showData(dataEntity.getList());
+                        }
+                    }
+                }
             }
 
             @Override
@@ -107,5 +156,81 @@ public class MyCommunityActivity extends BaseActivity {
             }
         });
     }
+
+    private void showData(List<CommunityItemEntity> listData) {
+        try {
+            if (listData.size() == 0) {
+                canLoadMore = false;
+                communityAdapter.loadMoreEnd();
+            } else {
+                page = page + 1;
+                canLoadMore = true;
+                communityAdapter.loadMoreComplete();
+                for (int i = 0; i < listData.size(); i++) {
+                    CommunityItemEntity item = listData.get(i);
+                    if (item != null) {
+                        listCommunity.add(item);
+                    }
+                }
+                communityAdapter.setNewData(listCommunity);
+                communityAdapter.notifyDataSetChanged();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class MyCommunityAdapter extends BaseQuickAdapter<CommunityItemEntity, BaseViewHolder> {
+
+        public MyCommunityAdapter(@LayoutRes int layoutResId, @Nullable List<CommunityItemEntity> data) {
+            super(layoutResId, data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, CommunityItemEntity item) {
+            TextView tvCommunityName = helper.getView(R.id.tv_community_name);
+            TextView tvIntroduction = helper.getView(R.id.tv_community_introduction);
+            tvCommunityName.setText(item.getName());
+            String introduction = item.getIntroduction();
+            if (TextUtils.isEmpty(introduction)) {
+                tvIntroduction.setText(getString(R.string.community_introduction_default));
+            } else {
+                tvIntroduction.setText(introduction);
+            }
+            final TagFlowLayout flTab = helper.getView(R.id.fl_tab);
+            String tagStr = item.getTags();
+            if (TextUtils.isEmpty(tagStr)) {
+                flTab.setVisibility(View.GONE);
+            } else {
+                flTab.setVisibility(View.VISIBLE);
+                String[] tagArr = tagStr.split(",");
+                if (tagArr != null && tagArr.length > 0) {
+                    final TagAdapter<String> adapter_Tag = new TagAdapter<String>(tagArr) {
+                        @Override
+                        public View getView(FlowLayout parent, int position, String item) {
+                            TextView tv = (TextView) getLayoutInflater().inflate(R.layout.item_tag_cannot_select,
+                                    flTab, false);
+                            if (!TextUtils.isEmpty(item)) {
+                                tv.setText(item);
+                            }
+                            return tv;
+                        }
+                    };
+                    flTab.setAdapter(adapter_Tag);
+                }
+            }
+        }
+    }
+
+    private BaseQuickAdapter.RequestLoadMoreListener onLoadMoreListener = new BaseQuickAdapter.RequestLoadMoreListener() {
+        @Override
+        public void onLoadMoreRequested() {
+            Logger.e("----->onLoadMoreRequested ");
+            Logger.e("--->page = " + page);
+            if (canLoadMore) {
+                getCommunityList();
+            }
+        }
+    };
 
 }
--- IMan/app/src/main/java/com/sundy/iman/ui/activity/MyContactsActivity.java
+++ IMan/app/src/main/java/com/sundy/iman/ui/activity/MyContactsActivity.java
@@ -1,14 +1,23 @@
 package com.sundy.iman.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.sundy.iman.R;
+import com.sundy.iman.entity.MemberInfoEntity;
+import com.sundy.iman.helper.UIHelper;
 import com.sundy.iman.interfaces.OnTitleBarClickListener;
 import com.sundy.iman.view.TitleBarView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,6 +35,9 @@ public class MyContactsActivity extends BaseActivity {
     @BindView(R.id.title_bar)
     TitleBarView titleBar;
 
+    private ContactAdapter contactAdapter;
+    private List<MemberInfoEntity> listContact = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +51,22 @@ public class MyContactsActivity extends BaseActivity {
     private void init() {
         rvContact.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
+        contactAdapter = new ContactAdapter(R.layout.item_contact, listContact);
+        View view_community = getLayoutInflater().inflate(R.layout.view_my_community_header, null);
+        view_community.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goMyCommunity();
+            }
+        });
+
+        contactAdapter.addHeaderView(view_community);
+        rvContact.setAdapter(contactAdapter);
+    }
+
+    //跳转我的社区列表
+    private void goMyCommunity() {
+        UIHelper.jump(this, MyCommunityActivity.class);
     }
 
     private void initTitle() {
@@ -71,4 +99,16 @@ public class MyContactsActivity extends BaseActivity {
         });
     }
 
+
+    private class ContactAdapter extends BaseQuickAdapter<MemberInfoEntity, BaseViewHolder> {
+
+        public ContactAdapter(@LayoutRes int layoutResId, @Nullable List<MemberInfoEntity> data) {
+            super(layoutResId, data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, MemberInfoEntity item) {
+
+        }
+    }
 }
