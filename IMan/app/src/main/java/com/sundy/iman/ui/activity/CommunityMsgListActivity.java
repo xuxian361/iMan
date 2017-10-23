374ec65da28ee5ad1f7ba28ed89a6e2d4337ea6c
Sundy <308379285@qq.com>
2017-10-23 16:08:22 +0800

    修改 社区消息列表

--- IMan/app/src/main/java/com/sundy/iman/ui/activity/CommunityMsgListActivity.java
+++ IMan/app/src/main/java/com/sundy/iman/ui/activity/CommunityMsgListActivity.java
@@ -3,15 +3,31 @@ package com.sundy.iman.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
 
+import com.sundy.iman.MainApp;
 import com.sundy.iman.R;
+import com.sundy.iman.config.Constants;
+import com.sundy.iman.entity.CommunityInfoEntity;
 import com.sundy.iman.helper.UIHelper;
 import com.sundy.iman.interfaces.OnTitleBarClickListener;
+import com.sundy.iman.net.ParamHelper;
+import com.sundy.iman.net.RetrofitCallback;
+import com.sundy.iman.net.RetrofitHelper;
+import com.sundy.iman.paperdb.PaperUtils;
 import com.sundy.iman.view.TitleBarView;
+import com.sundy.iman.view.popupwindow.CommunityMenuPopup;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Response;
 
 /**
  * 社区消息列表页面
@@ -24,8 +40,11 @@ public class CommunityMsgListActivity extends BaseActivity {
     TitleBarView titleBar;
     @BindView(R.id.rv_msg)
     RecyclerView rvMsg;
+    @BindView(R.id.ll_content)
+    LinearLayout llContent;
 
     private String community_id;
+    private CommunityMenuPopup communityMenuPopup;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,11 +61,10 @@ public class CommunityMsgListActivity extends BaseActivity {
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             community_id = bundle.getString("community_id");
+            if (!TextUtils.isEmpty(community_id)) {
+                getCommunityInfo();
+            }
         }
-    }
-
-    private void init() {
-
     }
 
     private void initTitle() {
@@ -65,7 +83,7 @@ public class CommunityMsgListActivity extends BaseActivity {
 
             @Override
             public void onRightImgClick() {
-
+                showMenuPopup();
             }
 
             @Override
@@ -80,11 +98,93 @@ public class CommunityMsgListActivity extends BaseActivity {
         });
     }
 
+    private void init() {
+        communityMenuPopup = new CommunityMenuPopup(this);
+        communityMenuPopup.setOnClickListener(clickListener);
+    }
+
+    //社区详情
+    private void getCommunityInfo() {
+        Map<String, String> param = new HashMap<>();
+        param.put("mid", PaperUtils.getMId());
+        param.put("session_key", PaperUtils.getSessionKey());
+        param.put("community_id", community_id); //社区ID
+        param.put("type", "1"); //类型: 1-普通社区，2-推广社区
+        Call<CommunityInfoEntity> call = RetrofitHelper.getInstance().getRetrofitServer()
+                .getCommunityInfo(ParamHelper.formatData(param));
+        call.enqueue(new RetrofitCallback<CommunityInfoEntity>() {
+            @Override
+            public void onSuccess(Call<CommunityInfoEntity> call, Response<CommunityInfoEntity> response) {
+                CommunityInfoEntity communityInfoEntity = response.body();
+                if (communityInfoEntity != null) {
+                    int code = communityInfoEntity.getCode();
+                    String msg = communityInfoEntity.getMsg();
+                    if (code == Constants.CODE_SUCCESS) {
+                        CommunityInfoEntity.DataEntity dataEntity = communityInfoEntity.getData();
+                        if (dataEntity != null) {
+                            titleBar.setBackMode(dataEntity.getName());
+                            titleBar.setRightIvVisibility(View.VISIBLE);
+                        }
+                    } else {
+                        MainApp.getInstance().showToast(msg);
+                    }
+                }
+            }
+
+            @Override
+            public void onAfter() {
+
+            }
+
+            @Override
+            public void onFailure(Call<CommunityInfoEntity> call, Throwable t) {
+
+            }
+        });
+    }
+
+    //显示菜单Popup
+    private void showMenuPopup() {
+        communityMenuPopup.showAtLocation(llContent, Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+    }
+
+    private CommunityMenuPopup.OnClickListener clickListener = new CommunityMenuPopup.OnClickListener() {
+        @Override
+        public void quitClick() {
+            communityMenuPopup.dismiss();
+        }
+
+        @Override
+        public void postClick() {
+            communityMenuPopup.dismiss();
+        }
+
+        @Override
+        public void infoClick() {
+            communityMenuPopup.dismiss();
+            goCommunityDetail(community_id);
+        }
+
+        @Override
+        public void shareClick() {
+            communityMenuPopup.dismiss();
+        }
+    };
+
     //跳转社区详情
     private void goCommunityDetail(String community_id) {
         Bundle bundle = new Bundle();
         bundle.putString("community_id", community_id);
         bundle.putString("type", "1"); //类型 1-普通社区,2- 推广社区
         UIHelper.jump(this, CommunityDetailActivity.class, bundle);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (communityMenuPopup != null) {
+            communityMenuPopup.dismiss();
+            communityMenuPopup = null;
+        }
     }
 }
---
+++ IMan/app/src/main/java/com/sundy/iman/view/popupwindow/CommunityMenuPopup.java
@@ -0,0 +1,99 @@
+package com.sundy.iman.view.popupwindow;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+
+import com.sundy.iman.R;
+
+/**
+ * Created by sundy on 17/10/23.
+ */
+
+public class CommunityMenuPopup extends PopupWindow implements View.OnClickListener {
+
+    private Context mContext;
+    private View view;
+
+    public CommunityMenuPopup(Context context) {
+        super(context);
+        this.mContext = context;
+        initPopup();
+        setPopupWindow();
+    }
+
+    private void initPopup() {
+        view = LayoutInflater.from(mContext).inflate(R.layout.popup_community_menu, null);
+
+        ImageView iv_close = (ImageView) view.findViewById(R.id.iv_close);
+        LinearLayout ll_post_msg = (LinearLayout) view.findViewById(R.id.ll_post_msg);
+        LinearLayout ll_community_info = (LinearLayout) view.findViewById(R.id.ll_community_info);
+        LinearLayout ll_share_community = (LinearLayout) view.findViewById(R.id.ll_share_community);
+        LinearLayout ll_quit_community = (LinearLayout) view.findViewById(R.id.ll_quit_community);
+
+        iv_close.setOnClickListener(this);
+        ll_post_msg.setOnClickListener(this);
+        ll_community_info.setOnClickListener(this);
+        ll_share_community.setOnClickListener(this);
+        ll_quit_community.setOnClickListener(this);
+
+    }
+
+    private void setPopupWindow() {
+        this.setContentView(view);
+        ColorDrawable dw = new ColorDrawable(0x00000000);
+        this.setBackgroundDrawable(dw);
+        this.setFocusable(true);
+        this.setOutsideTouchable(true);
+        this.setWidth(WindowManager.LayoutParams.MATCH_PARENT);
+        this.setHeight(WindowManager.LayoutParams.MATCH_PARENT);
+        this.setAnimationStyle(R.style.popWindow_anim_style);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_post_msg:
+                if (onClickListener != null)
+                    onClickListener.postClick();
+                break;
+            case R.id.ll_community_info:
+                if (onClickListener != null)
+                    onClickListener.infoClick();
+                break;
+            case R.id.ll_share_community:
+                if (onClickListener != null)
+                    onClickListener.shareClick();
+                break;
+            case R.id.ll_quit_community:
+                if (onClickListener != null)
+                    onClickListener.quitClick();
+                break;
+            case R.id.iv_close:
+                dismiss();
+                break;
+        }
+    }
+
+    public OnClickListener onClickListener;
+
+    public interface OnClickListener {
+        void quitClick();
+
+        void postClick();
+
+        void infoClick();
+
+        void shareClick();
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+}
